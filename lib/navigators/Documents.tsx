160e5844080dc8f4a83cc782d13359a9849cc3f2
kibebr <0763722@students.dadeschools.net>
2021-08-02 14:07:15 +0000

    removed unused TakeDocumentPicture route

--- lib/navigators/Documents.tsx
+++ lib/navigators/Documents.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { Document } from '../domain/Document'
 import { SeeDocumentScreen } from 'screens/Documents/SeeDocument.screen'
 import { CreateDocumentScreen } from 'screens/Documents/CreateDocument.screen'
-import { TakeDocumentPictureScreen } from 'screens/Documents/TakeDocumentPicture.screen'
 import { SeeAllDocumentsScreen } from 'screens/Documents/SeeAllDocuments.screen'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Button } from 'react-native-elements'
 import tailwind from 'tailwind-rn'
 
 export type DocumentsStackParamList = {
-  TakeDocumentPicture: { 
-    onPictureTaken: (picture: string) => void
-  }
   SeeDocument: { 
     document: Document
   }
@@ -43,13 +39,6 @@ export const DocumentsNavigator = (): JSX.Element => {
         component={CreateDocumentScreen}
         options={() => ({
           title: 'Create a document'
-        })}
-      />
-      <DocumentsStack.Screen
-        name='TakeDocumentPicture'
-        component={TakeDocumentPictureScreen}
-        options={() => ({
-          title: 'Take a picture'
         })}
       />
       <DocumentsStack.Screen
